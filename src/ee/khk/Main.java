55b1d6706341df5a6d91b7e5e22f31267eac177c
Snagrap <karlkarilaid04@gmail.com>
2021-03-30 16:48:40 +0300

    TereJavaFX - 3. Scene klass

--- src/ee/khk/Main.java
+++ src/ee/khk/Main.java
@@ -5,6 +5,10 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class Main extends Application{
     public static void main(String[] args) {
@@ -14,9 +18,19 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) {
+
+        //Label label = new Label("Hello");
+        //Button button = new Button("Button");
+        //Group group = new Group(button);
+
+        Group root = new Group();
+
+        //FlowPane root = new FlowPane(label, group);
+        Scene scene = new Scene(root, 400, 150, Color.BLUE);
+        stage.setScene(scene);
+
         stage.setTitle("Hello JavaFX");
-        stage.setWidth(250);
-        stage.setHeight(150);
+
         stage.show();
     }
 
