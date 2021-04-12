35985230fc93b3d39e124f03b8e6d103cf8150b1
Snagrap <karlkarilaid04@gmail.com>
2021-04-12 13:07:24 +0300

    TereJavaFX - 3 HBox

--- src/ee/khk/Main.java
+++ src/ee/khk/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -29,13 +29,17 @@ public class Main extends Application{
     public void start(Stage stage) {
         Label label1 = new Label("Label1");
         Label label2 = new Label("Label2");
+        Label label3 = new Label("Label3");
+        Label label4 = new Label("Label4");
+        Label label5 = new Label("Label5");
+        Label label6 = new Label("Label6");
+        Label label7 = new Label("Label7");
 
-        FlowPane root = new FlowPane(10,10, label1,label2);
-        root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 300, 150);
+        HBox root = new HBox(10, label1, label2, label3, label4, label5, label6, label7);
+        Scene scene = new Scene(root, 500, 150);
         stage.setScene(scene);
 
-        stage.setTitle("FlowPane in JavaFX");
+        stage.setTitle("HBox in JavaFX");
 
         stage.show();
 
