fcb995272343dea839f8626d25aa564ae4dc8228
Snagrap <karlkarilaid04@gmail.com>
2021-04-12 13:28:57 +0300

    TereJavaFX - 5 BorderPane

--- src/ee/khk/Main.java
+++ src/ee/khk/Main.java
@@ -5,14 +5,11 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
 import javafx.scene.Parent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
@@ -30,26 +27,12 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) {
 
-        Button okBtn = new Button("OK");
-        Button cancelBtn = new Button("Cancel");
-        Label lbl = new Label("Select");
+        Button btn = new Button("Hello");
+        BorderPane root = new BorderPane(btn);
 
-        VBox vbox = new VBox(15);
-
-        VBox.setVgrow(okBtn, Priority.ALWAYS);
-        okBtn.setMaxHeight(Double.MAX_VALUE);
-        okBtn.setMaxWidth(100);
-
-        VBox.setVgrow(cancelBtn, Priority.ALWAYS);
-        cancelBtn.setMaxHeight(Double.MAX_VALUE);
-        cancelBtn.setMaxWidth(100);
-
-        vbox.getChildren().addAll(lbl, okBtn, cancelBtn);
-        Scene scene = new Scene(vbox, 300, 150);
+        Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
-
-        stage.setTitle("VBox in JavaFX");
-
+        stage.setTitle("BorderPane in JavaFX");
         stage.show();
 
     }
