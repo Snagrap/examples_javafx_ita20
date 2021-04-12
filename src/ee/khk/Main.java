f629811791265621691129eea37f38396f85cd7d
Snagrap <karlkarilaid04@gmail.com>
2021-04-12 13:04:18 +0300

    TereJavaFX - 2.2 FlowPane

--- src/ee/khk/Main.java
+++ src/ee/khk/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -28,23 +29,13 @@ public class Main extends Application{
     public void start(Stage stage) {
         Label label1 = new Label("Label1");
         Label label2 = new Label("Label2");
-        Label label3 = new Label("Label3");
-        Label label4 = new Label("Label4");
-        Label label5 = new Label("Label5");
-        Label label6 = new Label("Label6");
-        Label label7 = new Label("Label7");
-        Label label8 = new Label("Label8");
-        Label label9 = new Label("Label9");
 
-        FlowPane root = new FlowPane();
-        root.setVgap(18);
-        root.setHgap(15);
-        root.setOrientation(Orientation.VERTICAL);
-        root.getChildren().addAll(label1,label2,label3,label4,label5,label6,label7,label8,label9);
+        FlowPane root = new FlowPane(10,10, label1,label2);
+        root.setAlignment(Pos.CENTER);
         Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
 
-        stage.setTitle("Flowpane in JavaFX");
+        stage.setTitle("FlowPane in JavaFX");
 
         stage.show();
 
