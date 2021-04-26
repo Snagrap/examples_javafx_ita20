1393150bda76ec29d4df1e25b68614645c8a21f6
Snagrap <karlkarilaid04@gmail.com>
2021-04-26 14:41:32 +0300

    TereJavaFX - 11. TreeView

--- src/ee/khk/Main.java
+++ src/ee/khk/Main.java
@@ -23,6 +23,10 @@ import javax.swing.*;
 import java.io.IOException;
 import javafx.scene.layout.FlowPane;
 import javafx.geometry.Insets;
+import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.SelectionMode;
 
 public class Main extends Application{
 
@@ -33,15 +37,46 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
-        ChoiceBox<String> langsChoiceBox = new ChoiceBox<String>(langs);
-        langsChoiceBox.setValue("Java");
+        TreeItem<String> rootTreeNode = new TreeItem<String>("Languages");
+        rootTreeNode.setExpanded(true);
+        TreeItem<String> germanics = new TreeItem<String>("Germanic");
+        germanics.getChildren().add(new TreeItem<String>("German"));
+        germanics.getChildren().add(new TreeItem<String>("English"));
+
+        TreeItem<String> romans = new TreeItem<String>("Roman");
+        romans.getChildren().add(new TreeItem<String>("French"));
+        romans.getChildren().add(new TreeItem<String>("Spanish"));
+        romans.getChildren().add(new TreeItem<String>("Italian"));
+
+        rootTreeNode.getChildren().add(germanics);
+        rootTreeNode.getChildren().add(romans);
+
+        TreeView<String> langsTreeView = new TreeView<String>(rootTreeNode);
+        langsTreeView.setPrefSize(150, 200);
+
+        MultipleSelectionModel<TreeItem<String>> selectionModel = langsTreeView.getSelectionModel();
+
         Label lbl = new Label();
-        langsChoiceBox.setOnAction(event -> lbl.setText(langsChoiceBox.getValue()));
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, langsChoiceBox, lbl);
+
+        selectionModel.selectedItemProperty().addListener(new ChangeListener<TreeItem<String>>() {
+            public void changed(ObservableValue<? extends TreeItem<String>> changed,
+                                TreeItem<String> oldValue, TreeItem<String> newValue){
+                if(newValue != null){
+                    String path = newValue.getValue();
+                    TreeItem<String> parent = newValue.getParent();
+                    while(parent != null){
+                        path = parent.getValue() + " / " + path;
+                        parent = parent.getParent();
+                    }
+                    lbl.setText(path);
+                }
+            }
+        });
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, langsTreeView, lbl);
         Scene scene = new Scene(root, 300, 250);
         stage.setScene(scene);
-        stage.setTitle("ChoiceBox in JavaFX");
+        stage.setTitle("TreeView in JavaFX");
         stage.show();
 
     }
