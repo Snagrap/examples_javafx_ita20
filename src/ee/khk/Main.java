132a0961d127d22ebd2038e9e09930c0851492bb
Snagrap <karlkarilaid04@gmail.com>
2021-04-26 13:56:42 +0300

    TereJavaFX - 3. ToggleButton

--- src/ee/khk/Main.java
+++ src/ee/khk/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.*;
 import javafx.scene.Parent;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -16,16 +17,12 @@ import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
 import javax.swing.*;
 import java.io.IOException;
+import javafx.scene.layout.FlowPane;
+import javafx.geometry.Insets;
 
 public class Main extends Application{
-
-    CheckBox java;
-    CheckBox javaScript;
-    CheckBox csharp;
-    Label selectedLangs;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -34,33 +31,25 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        selectedLangs = new Label("No languages selected");
-        java = new CheckBox("Java");
-        javaScript = new CheckBox("JavaScript");
-        csharp = new CheckBox("C#");
-        java.setOnAction(Event -> show());
-        javaScript.setOnAction(Event -> show());
-        csharp.setOnAction(Event -> show());
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 0, 10);
-        root.getChildren().addAll(java, javaScript, csharp, selectedLangs);
-        root.setPadding(new Insets(0,0,0,10));
+        ToggleButton lightBtn = new ToggleButton("Lights");
+        Label stateLbl = new Label();
+        lightBtn.setOnAction(event -> {
+            if(lightBtn.isSelected()) {
+                stateLbl.setText("Lights on!");
+            }
+            else{
+                stateLbl.setText("Lights off...");
+            }
+        });
+
+        FlowPane root = new FlowPane(10, 10);
+        root.getChildren().addAll(lightBtn, stateLbl);
+        root.setPadding(new Insets(10));
         Scene scene = new Scene(root, 250, 200);
         stage.setScene(scene);
         stage.setTitle("Hello JavaFX");
         stage.show();
 
-    }
-
-    private void show(){
-        String allSelected = "";
-        if(java.isSelected()) allSelected += "Java ";
-        if(javaScript.isSelected()) allSelected += "JavaScript ";
-        if(csharp.isSelected()) allSelected += "C#";
-
-        if(allSelected.equals(""))
-            selectedLangs.setText("No languages selected");
-        else
-            selectedLangs.setText(allSelected);
     }
 
 }
